ec96e1459f65a1ec54fc995afddd44e0dc7f6e0e
wudibin <781721794@qq.com>
2020-11-10 01:08:07 +0800

    [wu] font : finish ES6-Syntax homework

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+    let result = [];
+    input.data.forEach(p => {
+        let obj = {};
+        p.map((v, i) => {
+            obj[input.column[i].name] = v;
+        });
+        result.push(obj);
+    });
+    return result;
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+    sections.sort((a,b) => {
+        return b.index - a.index;
+    });
+    sections.forEach(s => {
+        items.splice(s.index, 0, s.content);
+    });
+    return items;
+};
+
 export { inject };
